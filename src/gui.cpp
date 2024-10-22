f4a01d44d093f0a23d620c1ef7e2a1833babdfcd
supa-hub <bilcutomi@gmail.com>
2024-10-22 16:37:06 +0300

    Modified settings for the CMake fo work with windows

--- src/gui.cpp
+++ src/gui.cpp
@@ -2,12 +2,22 @@
 
 #include <iostream>
 #include "backend/helper_tools.hpp"
+#include <memory>
+
 
 int main() 
 {
     std:: cout << helper::clamp<int>(5, 0, 6) << "\n";
     helper::Matrix<size_t> a(5, 5, 0);
 
+
+    std::weak_ptr<int> b;
+    {
+        std::shared_ptr<int> o = std::make_shared<int>(5); 
+        b = o;
+    }
+    
+    
 
     return 0;
 }
