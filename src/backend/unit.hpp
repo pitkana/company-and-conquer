618d1e58ef9e68a6dff2d6b18f319dbcbd9f89e3
Alpo Pitkänen <alpo.pitkanen@aalto.fi>
2024-11-05 14:16:58 +0200

    Create unit.hpp containing class Unit and related consts

---
+++ src/backend/unit.hpp
@@ -0,0 +1,32 @@
+#ifndef unit
+#define unit
+
+#include <array>
+
+struct unit_consts {
+    uint_8t max_hp = 10;
+    uint_8t move_range = 5;
+    uint_8t visual_range = 5;
+    uint_8t inventory_size = 4;
+}
+
+
+class Unit
+{
+
+public:
+    Unit(std::string &name) :
+        name_(name) {};
+    
+
+
+private:
+    std::string name_;
+
+    std::array<uint_8t, unit_consts.inventory_size> inventory;
+    uint_8t current_hp = unit_consts.max_hp;
+
+
+}
+
+#endif
