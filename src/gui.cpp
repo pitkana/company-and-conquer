f3aaf07d816965055ce5db6035bf3bd85fe455cf
langsjo <104687438+langsjo@users.noreply.github.com>
2024-11-11 12:49:02 +0200

    Added testing for movement check along with changes to map that allow some of its members to be accessed better

--- src/gui.cpp
+++ src/gui.cpp
@@ -12,16 +12,6 @@
 
 int main() 
 {
-    Game game;
-    game.add_action(Action::CharacterAction(0.5, 20, coordinates<size_t>(5, 3)));
-    game.add_action(Action::BuildingAction("turret", coordinates<size_t>(15, 1)));
-    game.add_action(Action::CharacterAction(0.3, 50, coordinates<size_t>(2, 10)));
-
-    game.execute_actions();
-
-    std::string ok = "Joo";
-    Unit juu(ok);
-    std::cout << juu.GetHP() << std::endl;
     return 0;
 }
 
