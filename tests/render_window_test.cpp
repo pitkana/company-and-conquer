07fad6e7506a161ae5289291163130014f5a88c8
Matias Häkkinen <matias.e.hakkinen@aalto.fi>
2024-11-26 17:25:57 +0200

    Added tests for Render_Window class.

---
+++ tests/render_window_test.cpp
@@ -0,0 +1,16 @@
+#include "render_window_test.hpp"
+#include "render_window.hpp"
+#include "map_builder.hpp"
+#include "game.hpp"
+
+void render_window_test() {
+    Map_Builder builder = Map_Builder();
+    std::vector<std::vector<char>> terrain_vec = builder.read_map_file(TESTMAP_PATH);
+    size_t test_map_height = terrain_vec.size();
+    size_t test_map_width = terrain_vec[0].size();
+    Game g(test_map_height,test_map_width);
+    Game* game = &g;
+    builder.load(terrain_vec,game->get_map());
+    Render_Window wind(game,TEXTURE_PATH,TESTMAP_PATH);
+    wind.spawn_window(700,500);
+}
