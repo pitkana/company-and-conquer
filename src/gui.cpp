ed11170f70c76abce14be1f9424e96f4747a1737
langsjo <104687438+langsjo@users.noreply.github.com>
2024-12-13 22:48:19 +0200

    Renamed the executable to main and made it actually be the game

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,26 +1,9 @@
-#include <iostream>
-#include <memory>
-#include <SFML/Window.hpp>
-
-#include "coordinates.hpp"
-#include "game.hpp"
-#include "action.hpp"
-#include "coordinates.hpp"
-#include <random>
-
 #include "renderer.hpp"
-
-
-
-
 
 int main()
 {
-    //start(1000, 1000);
-    Renderer renderer(1000, 1000);
-    renderer.initialise_level(0);
-    renderer.start();
-    
-
+    Renderer renderer = Renderer(1200, 700);
+    // renderer.initialize_scenario();
+    renderer.start_main_screen();
     return 0;
 }
